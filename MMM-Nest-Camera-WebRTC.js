8440cf4bea06ddaf5abcc5e08f81b6f252be4519
brandaorafael <r.augusto.brandao@gmail.com>
2025-03-02 21:22:22 -0500

    Update WebRTC module with improved start and stream handling

--- MMM-Nest-Camera-WebRTC.js
+++ MMM-Nest-Camera-WebRTC.js
@@ -1,160 +1,174 @@
 Module.register("MMM-Nest-Camera-WebRTC", {
-    video: null,
-    pc: null,
-    stream: null,
-    connectTimeout: null,
+	video: null,
+	pc: null,
+	stream: null,
+	connectTimeout: null,
 
-    suspended: false,
-    suspendedForUserPresence: false,
+	suspended: false,
+	suspendedForUserPresence: false,
 
-    defaults: {
-        width: "50%",
-        token: "",
-        url: "",
-    },
+	defaults: {
+		width: "50%",
+		token: "",
+		url: ""
+	},
 
-    start: function () {
-        if (this.data.hiddenOnStartup) {
-            // Don't connect if module is going to be hidden
-            this.suspended = true;
-            return;
-        }
-        this.initializeRTCPeerConnection();
-    },
+	first: true,
 
-    suspend: function () {
-        this.suspended = true;
-        if (this.stream) {
-            this.stream.getTracks().forEach(track => {
-                track.stop();
-            });
-            this.pc.close();
-            this.pc = null;
-            this.video.srcObject = null;
-            this.stream = null;
-        }
-    },
+	async start() {
+		if (this.data.hiddenOnStartup) {
+			// Don't connect if module is going to be hidden
+			this.suspended = true;
+			return;
+		}
+		await this.initializeRTCPeerConnection();
+	},
 
-    resume: function () {
-        this.suspended = false;
-        this.initializeRTCPeerConnection();
-        this.updateDom();
-    },
+	suspend() {
+		this.suspended = true;
+		if (this.stream) {
+			this.stream.getTracks().forEach((track) => {
+				track.stop();
+			});
+			this.pc.close();
+			this.pc = null;
+			this.video.srcObject = null;
+			this.stream = null;
+		}
+	},
 
-    getStyles: function () {
-        return [this.name + ".css"];
-    },
+	async resume() {
+		this.suspended = false;
+		await this.initializeRTCPeerConnection();
+		this.updateDom();
+	},
 
-    getDom: function () {
-        if (this.stream) {
-            this.video = document.createElement("video");
-            this.video.classList.add("rtw-video");
-            this.video.autoplay = true;
-            this.video.controls = false;
-            this.video.volume = 1;
-            this.video.muted = true;
-            this.video.style.maxWidth = this.config.width;
-            this.video.playsInline = true;
-            this.video.srcObject = this.stream;
+	getStyles() {
+		return [`${this.name}.css`];
+	},
 
-            const recover = () => {
-                this.video.srcObject = this.stream;
-                this.video.play();
-            };
-            this.video.onstalled = recover;
-            this.video.onerror = recover;
+	getDom() {
+		if (this.stream) {
+			this.video = document.createElement("video");
+			this.video.classList.add("rtw-video");
+			this.video.autoplay = true;
+			this.video.controls = false;
+			this.video.volume = 1;
+			this.video.muted = true;
+			this.video.style.maxWidth = this.config.width;
+			this.video.playsInline = true;
+			this.video.srcObject = this.stream;
 
-            return this.video;
-        }
+			const recover = () => {
+				this.video.srcObject = this.stream;
+				this.video.play();
+			};
+			this.video.onstalled = recover;
+			this.video.onerror = recover;
 
-        const error = document.createElement("div");
-        error.classList.add("rtw-error", "small");
-        error.innerHTML = "No data from stream";
-        return error;
-    },
+			return this.video;
+		}
 
-    notificationReceived: function(notification, payload, sender) {
-        // Handle USER_PRESENCE events from the MMM-PIR-sensor/similar modules
-        if (notification === "USER_PRESENCE") {
-            if (payload) {
-                this.suspendedForUserPresence = false;
-                if (this.suspended && !this.hidden) {
-                    this.resume();
-                }
-                return;
-            } else {
-                this.suspendedForUserPresence = true;
-                if (!this.suspended) {
-                    this.suspend();
-                }
-            }
-        }
-    },
+		const error = document.createElement("div");
+		error.classList.add("rtw-error", "small");
+		error.innerHTML = "No data from stream";
+		return error;
+	},
 
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === `ANSWER_${this.identifier}`) {
-            console.log(`${this.name} received answer for ${this.identifier}`);
-            try {
-                this.pc.setRemoteDescription(
-                new RTCSessionDescription({ type: 'answer', sdp: atob(payload) })
-                )
-            } catch (e) {
-                console.warn(e)
-            }
-            return;
-        }
-    },
+	async notificationReceived(notification, payload, sender) {
+		// Handle USER_PRESENCE events from the MMM-PIR-sensor/similar modules
+		if (notification === "USER_PRESENCE") {
+			if (payload) {
+				this.suspendedForUserPresence = false;
+				if (this.suspended && !this.hidden) {
+					await this.resume();
+				}
+			} else {
+				this.suspendedForUserPresence = true;
+				if (!this.suspended) {
+					this.suspend();
+				}
+			}
+		}
+	},
 
-    async initializeRTCPeerConnection() {
-        console.log(`${this.name} initializing connection for ${this.identifier}`);
+	async socketNotificationReceived(notification, payload) {
+		if (notification === `ANSWER_${this.identifier}`) {
+			Log.log(`${this.name} received answer for ${this.identifier}`);
+			try {
+				await this.pc.setRemoteDescription(
+					new RTCSessionDescription({type: "answer", sdp: payload})
+				);
+			} catch (e) {
+				console.warn(e);
+			}
+		}
+	},
 
-        this.stream = new MediaStream();
-        this.pc = new RTCPeerConnection({
-            iceServers: [
-                {
-                    urls: ["stun:stun.l.google.com:19302"],
-                },
-            ],
-            sdpSemantics: 'unified-plan'
-        });
+	async initializeRTCPeerConnection() {
+		Log.log(`${this.name} initializing connection for ${this.identifier}`);
 
-        this.pc.onconnectionstatechange = () => {
-            if (this.pc.connectionState === "failed") {
-                console.log(`${this.name} connection in failed state, restarting`);
-                this.pc.close();
-                this.video.srcObject = null;
-                this.initializeRTCPeerConnection();
-            }
-        };
+		this.stream = new MediaStream();
+		this.pc = new RTCPeerConnection({
+			iceServers: [
+				{
+					urls: ["stun:stun.l.google.com:19302"]
+				}
+			],
+			sdpSemantics: "unified-plan"
+		});
 
-        this.pc.ontrack = (event) => {
-            this.stream.addTrack(event.track);
-        };
+		this.pc.onconnectionstatechange = () => {
+			if (this.pc.connectionState === "failed") {
+				console.log(`${this.name} connection in failed state, restarting`);
+				this.pc.close();
+				this.video.srcObject = null;
+				this.initializeRTCPeerConnection();
+			}
+		};
 
-        const pingChannel = this.pc.createDataChannel("ping");
-        let intervalId;
-        pingChannel.onopen = () => {
-            intervalId = setInterval(() => {
-                try {
-                    pingChannel.send("ping");
-                } catch (e) {
-                    console.warn(e);
-                }
-            }, 1000);
-        };
-        pingChannel.onclose = () => {
-            clearInterval(intervalId);
-            if (this.suspended) { return; }  // Closed due to module being hidden
-            console.log(`${this.name} ping channel closed; restarting...`);
-            this.initializeRTCPeerConnection();
-        };
+		this.pc.ontrack = (event) => {
+			this.stream.addTrack(event.track);
+		};
 
-        this.pc.addTransceiver("video", { direction: "recvonly" });
-        this.pc.onnegotiationneeded = async () => {
-            const offer = await this.pc.createOffer();
-            await this.pc.setLocalDescription(offer);
+		const pingChannel = this.pc.createDataChannel("ping");
+		let intervalId;
+		pingChannel.onopen = () => {
+			intervalId = setInterval(() => {
+				try {
+					this.sendSocketNotification("EXTEND_STREAM", {
+						token: this.config.token,
+						identifier: this.identifier,
+						nestProjectId: this.config.nestProjectId,
+						nestDeviceId: this.config.nestDeviceId
+					});
+				} catch (e) {
+					console.warn(e);
+				}
+			}, 300000);
+		};
+		pingChannel.onclose = () => {
+			clearInterval(intervalId);
+			if (this.suspended) {
+				return;
+			} // Closed due to module being hidden
+			console.log(`${this.name} ping channel closed; restarting...`);
+			this.initializeRTCPeerConnection();
+		};
 
-            this.sendSocketNotification("OFFER", { url: this.config.url, sdp: btoa(this.pc.localDescription.sdp), identifier: this.identifier })
-          }
-    },
+		this.pc.addTransceiver("audio", {direction: "recvonly"});
+		this.pc.addTransceiver("video", {direction: "recvonly"});
+		this.pc.onnegotiationneeded = async () => {
+			const offer = await this.pc.createOffer();
+			await this.pc.setLocalDescription(offer);
+
+			this.sendSocketNotification("START_STREAM", {
+				token: this.config.token,
+				sdp: this.pc.localDescription.sdp,
+				identifier: this.identifier,
+				nestProjectId: this.config.nestProjectId,
+				nestDeviceId: this.config.nestDeviceId
+			});
+		};
+	}
 });
--- node_helper.js
+++ node_helper.js
@@ -1,28 +1,63 @@
-const NodeHelper = require('node_helper');
-const fetch = require('node-fetch');
-const { URLSearchParams } = require('url');
+const NodeHelper = require("node_helper");
+const fetch = require("node-fetch");
 
+const Log = require("logger");
+
+let mediaSessionId;
 module.exports = NodeHelper.create({
-    start: function() {
-        console.log('Starting node_helper for module [' + this.name + ']');
-    },
+	start() {
+		Log.info(`Starting node_helper for module [${this.name}]`);
+	},
 
-    sendOffer: function(payload) {
-        console.log(`Offer requested for ${payload.identifier}`);
-        fetch(payload.url, {
-            method: 'POST',
-            body: new URLSearchParams({ data: payload.sdp })
-          })
-            .then(response => response.text())
-            .then(response_data => this.sendSocketNotification(`ANSWER_${payload.identifier}`, response_data))
-            .catch(error=>{
-                console.error(error);
-            });
-    },
+	async sendOffer(payload) {
+		const response = await fetch(`https://smartdevicemanagement.googleapis.com/v1/enterprises/${payload.nestProjectId}/devices/${payload.nestDeviceId}:executeCommand`,
+			{
+				headers: {
+					Authorization: `Bearer ${payload.token}`,
+					"Content-Type": "application/json"
+				},
+				method: "POST",
+				body: JSON.stringify({
+					command: "sdm.devices.commands.CameraLiveStream.GenerateWebRtcStream",
+					params: {
+						offerSdp: payload.sdp
+					}
+				})
+			});
 
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === 'OFFER') {
-            this.sendOffer(payload);
-        }
-    }
+		const resBody = await response.json();
+
+		mediaSessionId = resBody.results.mediaSessionId;
+		Log.info(`Media session id: ${mediaSessionId} (expires ${resBody.results.expiresAt})`);
+
+		this.sendSocketNotification(`ANSWER_${payload.identifier}`, resBody.results.answerSdp);
+	},
+
+	async extendStream(payload) {
+		await fetch(`https://smartdevicemanagement.googleapis.com/v1/enterprises/${payload.nestProjectId}/devices/${payload.nestDeviceId}:executeCommand`,
+			{
+				headers: {
+					Authorization: `Bearer ${payload.token}`,
+					"Content-Type": "application/json"
+				},
+				method: "POST",
+				body: JSON.stringify({
+					command: "sdm.devices.commands.CameraLiveStream.ExtendWebRtcStream",
+					params: {
+						mediaSessionId: mediaSessionId
+					}
+				})
+			});
+	},
+
+	async socketNotificationReceived(notification, payload) {
+		switch (notification) {
+			case "START_STREAM":
+				await this.sendOffer(payload);
+				break;
+			case "EXTEND_STREAM":
+				await this.extendStream(payload);
+				break;
+		}
+	}
 });
